87de36a326e8478f4e5f867876d03bd9f256120e
Diya Singla <diyasingla2004@gmail.com>
2023-05-12 15:47:12 +0530

    initial commit

---
+++ lib/chat_screen.dart
@@ -0,0 +1,99 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ChatBotScreen extends StatefulWidget {
+  @override
+  _ChatBotScreenState createState() => _ChatBotScreenState();
+}
+
+class _ChatBotScreenState extends State<ChatBotScreen> {
+  final List<Message> _messages = [];
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  void onSendMessage() async {
+    Message message = Message(text: _textEditingController.text, isMe: true);
+
+    if (message.text.isEmpty) return;
+
+    _textEditingController.clear();
+
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  Widget _buildMessage(Message message) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          child: Column(
+            crossAxisAlignment: message.isMe
+                ? CrossAxisAlignment.end
+                : CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                message.isMe ? 'You' : 'Bot',
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(message.text),
+            ],
+          )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ChatBot'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                  reverse: true,
+                  itemCount: _messages.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _buildMessage(_messages[index]);
+                  }),
+            ),
+            const Divider(
+              height: 1.0,
+            ),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: _textEditingController,
+                      decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.all(10.0),
+                        hintText: 'Type a Message...',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: onSendMessage,
+                    icon: const Icon(Icons.send),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class Message {
+  final String text;
+  final bool isMe;
+
+  Message({required this.text, required this.isMe});
+}
