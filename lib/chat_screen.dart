c5d3bca6050572028d5f319fcd39ba44b07100a9
Diya <diyasingla2004@gmail.com>
2024-02-21 18:43:06 +0530

    model retrained

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -14,6 +14,13 @@ class _ChatBotScreenState extends State<ChatBotScreen> {
 
   final TextEditingController _textEditingController = TextEditingController();
 
+  @override
+  void initState() {
+    super.initState();
+    _messages.add(
+        Message(text: "Hi, I am FinGenie. How may I assist you?", isMe: false));
+  }
+
   void onSendMessage() async {
     Message message = Message(text: _textEditingController.text, isMe: true);
 
@@ -35,38 +42,46 @@ class _ChatBotScreenState extends State<ChatBotScreen> {
   }
 
   Future<String> getResponse(String message) async {
+    String apiUrl = 'http://127.0.0.1:5000/api?input=$message';
+    final response = await http.get(Uri.parse(apiUrl));
 
-  String apiUrl = 'http://127.0.0.1:5000/api?input=$message';
-  final response = await http.get(Uri.parse(apiUrl));
-
-  if (response.statusCode == 200) {
-    final jsonResponse = json.decode(response.body);
-    return jsonResponse['response'];
-    
-  } 
-  else {
-    throw Exception('Failed to load response');
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      return jsonResponse['response'];
+    } else {
+      throw Exception('Failed to load response');
+    }
   }
-
-}
 
   Widget _buildMessage(Message message) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      margin: EdgeInsets.symmetric(vertical: 10.0),
       child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-          child: Column(
-            crossAxisAlignment: message.isMe
-                ? CrossAxisAlignment.end
-                : CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                message.isMe ? 'You' : 'Bot',
-                style: const TextStyle(fontWeight: FontWeight.bold),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        child: Column(
+          crossAxisAlignment:
+              message.isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              message.isMe ? 'You' : 'Bot',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: message.isMe
+                    ? Color.fromARGB(255, 228, 157, 241)
+                    : Color.fromARGB(255, 246, 171, 196),
+                borderRadius: BorderRadius.circular(8.0),
               ),
-              Text(message.text),
-            ],
-          )),
+              padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+              child: Text(
+                message.text,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
